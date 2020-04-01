6c7c3583b3ab49f3557d9eaf81503d0a88e7d4ce
Jonathan Darrer <jonny@jondarrer.me.uk>
2020-04-01 08:48:19 +0100

    15 :white_check_mark: Add test to check assets are added
    https://trello.com/c/IO23M7SQ/15-integrate-with-webpack

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,10 +1,41 @@
 /* global describe it expect */
+const ReactStaticSiteHydrater = require('./');
 
-describe('One is one and two is two', () => {
-  it('One is one', () => {
-    expect('One').toBe('One');
-  });
-  it('Two is two', () => {
-    expect('Two').toBe('Two');
+describe('ReactStaticSiteHydrater', () => {
+  const tapAsync = jest.fn();
+  const compiler = {
+    hooks: {
+      emit: {
+        tapAsync,
+      },
+    },
+  };
+  const createCompilation = (assets) => {
+    return {
+      assets,
+      getAsset: (name) => assets[name],
+    };
+  };
+  const callback = jest.fn();
+  describe('apply', () => {
+    beforeEach(() => {
+      tapAsync.mockClear();
+      callback.mockClear();
+    });
+    it('it should add the configured routes as assets', (done) => {
+      assets = {
+        'index.html': { source: { source: () => {} }, size: () => {} },
+      };
+      tapAsync.mockImplementation((name, emitCB) => {
+        expect(name).toBe('ReactStaticSiteHydrater');
+        emitCB(createCompilation(assets), callback);
+        expect(callback).toBeCalled();
+        expect(assets['__index.html']).toBeDefined();
+        expect(assets['__about.html']).toBeDefined();
+        done();
+      });
+      const sut = new ReactStaticSiteHydrater({ routes: ['/', '/about'] });
+      sut.apply(compiler);
+    });
   });
 });
